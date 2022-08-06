2d1d7f743452423f5659042aa7c01231b5f0ba39
arooj khan <aroojkhan.wr@gmail.com>
2022-08-06 16:36:11 -0400

    lattest

--- src/main/java/com/example/isdb/config/SecurityConfiguration.java
+++ src/main/java/com/example/isdb/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -46,6 +47,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
     }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("static/**");
+//    }
+
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
